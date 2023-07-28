55f5f02d2e8a52c667a9d50e788607d97a109f4b
HannahMidd <hmiddleton1017@gmail.com>
2023-07-28 15:47:27 -0400

    adding carousel fixes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const carouselInner = document.querySelector('.carousel-inner');
     const cardWidth = document.querySelector('.carousel-item').clientWidth;
     const totalCards = document.querySelectorAll('.carousel-item').length;
+    let scrollPosition = 0;
 
     arrowButton.addEventListener('click', function () {
         // Calculate the offsetTop of the about section relative to the document
@@ -17,68 +18,82 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-    if (window.matchMedia('(min-width: 768px)').matches) {
-        let scrollPosition = 0;
+    updateArrowVisibility();
 
-        updateArrowVisibility();
+    document
+        .getElementById('carouselExampleControls')
+        .addEventListener('slide.bs.carousel', function () {
+            setTimeout(updateArrowVisibility, 500); // Delay the update to ensure the carousel has finished sliding
+        });
 
-        document
-            .getElementById('carouselExampleControls')
-            .addEventListener('slide.bs.carousel', function () {
-                setTimeout(updateArrowVisibility, 500); // Delay the update to ensure the carousel has finished sliding
-            });
+    function updateArrowVisibility() {
+        const carouselWidth = carouselInner.scrollWidth;
+        const remainingScrollWidth =
+            carouselWidth - scrollPosition - carouselInner.clientWidth;
 
-        function updateArrowVisibility() {
-            const carouselWidth = carouselInner.scrollWidth;
-            const remainingScrollWidth =
-                carouselWidth - scrollPosition - carouselInner.clientWidth;
+        const tolerance = 5; // Set a small tolerance value (adjust as needed)
 
-            if (remainingScrollWidth <= cardWidth) {
-                document.querySelector('.carousel-control-next').style.display =
-                    'none';
-            } else {
-                document.querySelector('.carousel-control-next').style.display =
-                    'block';
-            }
+        // Hide/show next button based on remaining scroll width
+        if (remainingScrollWidth <= tolerance) {
+            document.querySelector('.carousel-control-next').style.display =
+                'none';
+        } else {
+            document.querySelector('.carousel-control-next').style.display =
+                'block';
         }
 
-        $('#carouselExampleControls .carousel-control-next').on(
-            'click',
-            function () {
-                if (
-                    scrollPosition <
-                    carouselInner.scrollWidth - carouselInner.clientWidth
-                ) {
-                    scrollPosition += cardWidth;
-                    $('#carouselExampleControls .carousel-inner').animate(
-                        { scrollLeft: scrollPosition },
-                        600
-                    );
-                }
-                updateArrowVisibility();
-            }
-        );
+        // Hide/show previous button based on scroll position
+        if (scrollPosition <= tolerance) {
+            document.querySelector('.carousel-control-prev').style.display =
+                'none';
+        } else {
+            document.querySelector('.carousel-control-prev').style.display =
+                'block';
+        }
 
-        $('#carouselExampleControls .carousel-control-prev').on(
-            'click',
-            function () {
-                if (scrollPosition > 0) {
-                    scrollPosition -= cardWidth;
-                    $('#carouselExampleControls .carousel-inner').animate(
-                        { scrollLeft: scrollPosition },
-                        600
-                    );
-                }
-                updateArrowVisibility();
-            }
-        );
-    } else {
-        $(carousel).addClass('slide');
+        // Adjust scroll position if it goes out of bounds
+        if (scrollPosition > carouselWidth - carouselInner.clientWidth) {
+            scrollPosition = carouselWidth - carouselInner.clientWidth;
+            $('#carouselExampleControls .carousel-inner').animate(
+                { scrollLeft: scrollPosition },
+                200
+            );
+        }
     }
+
+    $('#carouselExampleControls .carousel-control-next').on(
+        'click',
+        function () {
+            if (
+                scrollPosition <
+                carouselInner.scrollWidth - carouselInner.clientWidth
+            ) {
+                scrollPosition += cardWidth;
+                $('#carouselExampleControls .carousel-inner').animate(
+                    { scrollLeft: scrollPosition },
+                    600
+                );
+            }
+            updateArrowVisibility();
+        }
+    );
+
+    $('#carouselExampleControls .carousel-control-prev').on(
+        'click',
+        function () {
+            if (scrollPosition > 0) {
+                scrollPosition -= cardWidth;
+                $('#carouselExampleControls .carousel-inner').animate(
+                    { scrollLeft: scrollPosition },
+                    600
+                );
+            }
+            updateArrowVisibility();
+        }
+    );
 });
 
 // Arrow button to scroll to about section
-
 document.addEventListener('DOMContentLoaded', function () {
     const arrowButton = document.getElementById('arrow-btn');
     const aboutSection = document.getElementById('about');
