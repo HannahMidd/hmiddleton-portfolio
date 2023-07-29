0c4f60e7d39ac02ec407d4315cec38366b57c717
HannahMidd <hmiddleton1017@gmail.com>
2023-07-28 20:46:07 -0400

    adding corrected JS for media queries

--- index.js
+++ index.js
@@ -3,15 +3,10 @@ document.addEventListener('DOMContentLoaded', function () {
     const aboutSection = document.getElementById('about');
     const carousel = document.querySelector('#carouselExampleControls');
     const carouselInner = document.querySelector('.carousel-inner');
-    const cardWidth = document.querySelector('.carousel-item').clientWidth;
-    const totalCards = document.querySelectorAll('.carousel-item').length;
     let scrollPosition = 0;
 
     arrowButton.addEventListener('click', function () {
-        // Calculate the offsetTop of the about section relative to the document
         const offsetTop = aboutSection.offsetTop;
-
-        // Scroll the page to the about section with smooth behavior
         window.scrollTo({
             top: offsetTop,
             behavior: 'smooth',
@@ -23,33 +18,19 @@ document.addEventListener('DOMContentLoaded', function () {
     document
         .getElementById('carouselExampleControls')
         .addEventListener('slide.bs.carousel', function () {
-            setTimeout(updateArrowVisibility, 500); // Delay the update to ensure the carousel has finished sliding
+            setTimeout(updateArrowVisibility, 500);
         });
 
     function updateArrowVisibility() {
         const carouselWidth = carouselInner.scrollWidth;
         const remainingScrollWidth =
             carouselWidth - scrollPosition - carouselInner.clientWidth;
+        const tolerance = 5;
 
-        const tolerance = 5; // Set a small tolerance value (adjust as needed)
-
-        // Hide/show next button based on remaining scroll width
-        if (remainingScrollWidth <= tolerance) {
-            document.querySelector('.carousel-control-next').style.display =
-                'none';
-        } else {
-            document.querySelector('.carousel-control-next').style.display =
-                'block';
-        }
-
-        // Hide/show previous button based on scroll position
-        if (scrollPosition <= tolerance) {
-            document.querySelector('.carousel-control-prev').style.display =
-                'none';
-        } else {
-            document.querySelector('.carousel-control-prev').style.display =
-                'block';
-        }
+        document.querySelector('.carousel-control-next').style.display =
+            remainingScrollWidth <= tolerance ? 'none' : 'block';
+        document.querySelector('.carousel-control-prev').style.display =
+            scrollPosition <= tolerance ? 'none' : 'block';
 
         // Adjust scroll position if it goes out of bounds
         if (scrollPosition > carouselWidth - carouselInner.clientWidth) {
@@ -68,6 +49,12 @@ document.addEventListener('DOMContentLoaded', function () {
                 scrollPosition <
                 carouselInner.scrollWidth - carouselInner.clientWidth
             ) {
+                // Get the width of the next carousel item dynamically
+                const cardWidth = $(
+                    '#carouselExampleControls .carousel-item.active'
+                )
+                    .next()
+                    .width();
                 scrollPosition += cardWidth;
                 $('#carouselExampleControls .carousel-inner').animate(
                     { scrollLeft: scrollPosition },
@@ -82,6 +69,12 @@ document.addEventListener('DOMContentLoaded', function () {
         'click',
         function () {
             if (scrollPosition > 0) {
+                // Get the width of the previous carousel item dynamically
+                const cardWidth = $(
+                    '#carouselExampleControls .carousel-item.active'
+                )
+                    .prev()
+                    .width();
                 scrollPosition -= cardWidth;
                 $('#carouselExampleControls .carousel-inner').animate(
                     { scrollLeft: scrollPosition },
@@ -91,21 +84,4 @@ document.addEventListener('DOMContentLoaded', function () {
             updateArrowVisibility();
         }
     );
-});
-
-// Arrow button to scroll to about section
-document.addEventListener('DOMContentLoaded', function () {
-    const arrowButton = document.getElementById('arrow-btn');
-    const aboutSection = document.getElementById('about');
-
-    arrowButton.addEventListener('click', function () {
-        // Calculate the offsetTop of the about section relative to the document
-        const offsetTop = aboutSection.offsetTop;
-
-        // Scroll the page to the about section with smooth behavior
-        window.scrollTo({
-            top: offsetTop,
-            behavior: 'smooth',
-        });
-    });
 });
